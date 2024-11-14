5e72a6b17dd15252dee29487d0e1b70a4f71ea0c
Esteban Cadiz <estebancadiz@gmail.com>
2024-11-14 16:29:12 -0300

    initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ProyectoConsultorio
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
